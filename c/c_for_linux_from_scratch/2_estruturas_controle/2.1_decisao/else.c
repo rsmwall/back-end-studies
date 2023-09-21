eb08f318d694aba4a0c8fe9372f59c170bdd47e9
Rafael <rafaelrsilva.dev@gmail.com>
2023-09-21 02:19:13 -0300

    Estruturas de fluxo em C

---
+++ c/c_for_linux_from_scratch/2_estruturas_controle/2.1_decisao/else.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int numero = 3;
+
+    if (numero > 5) {
+        printf("O numero eh maior do que 5.\n");
+    } else {
+        printf("O numero nao eh maior do que 5.\n");
+    }
+
+    return 0;
+}
---
+++ c/c_for_linux_from_scratch/2_estruturas_controle/2.2_repeticao/while.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int contador = 0;
+
+    while (contador < 5) {
+        printf("Contador: %d\n", contador);
+        contador++;
+    }
+}

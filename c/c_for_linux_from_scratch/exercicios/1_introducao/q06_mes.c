ce5aaf7bcea00548b80311ac2a49bd588fbbb233
Rafael <rafaelrsilva.dev@gmail.com>
2023-09-21 02:37:46 -0300

    Exercicios de introducao a C

---
+++ c/c_for_linux_from_scratch/exercicios/1_introducao/q06_mes.c
@@ -0,0 +1,11 @@
+// Declare uma variável char para armazenar o nome de um mês. Escolha um nome de variável que seja descritivo e significativo.
+
+#include <stdio.h>
+
+int main() {
+    char mes[10] = "Outubro";
+
+    printf("O mes eh %s", mes);
+
+    return 0;
+}

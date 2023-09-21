ffcc2eb3a55cb01571a81437e07650cbb881e7df
Rafael <rafaelrsilva.dev@gmail.com>
2023-09-21 02:10:03 -0300

    Introducao a  C
    Variaveis e tipos de dados, entrada e saida.

---
+++ c/c-for-linux-from-scratch/1-introducao/1.3-entrada-saida/entrada.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+
+    int numero;
+    printf("Digite um numero inteiro: ");
+    scanf("%d", &numero);
+    printf("Voce digitou %d\n", numero);
+
+    return 0;
+}

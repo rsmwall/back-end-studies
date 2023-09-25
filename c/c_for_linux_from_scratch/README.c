d37adb82a9bb3ce52bd6d7520361c405f688e0b9
Rafael <rafaelrsilva.dev@gmail.com>
2023-09-25 00:30:34 -0300

    Create README.c

---
+++ c/c_for_linux_from_scratch/README.c
@@ -0,0 +1,93 @@
+## Cronograma utilizado
+
+**Aula 1: Introdução à Linguagem C**
+
+Nesta aula, vamos cobrir os conceitos fundamentais da linguagem C.
+
+**1.1 O que é C?**
+   - Introdução à história e importância da linguagem C.
+   - Comparação com outras linguagens de programação.
+
+**1.2 Configurando seu ambiente de desenvolvimento**
+   - Instalação de um compilador C (como GCC) no seu sistema.
+   - Configuração de um ambiente de desenvolvimento integrado (IDE) ou uso de um editor de texto.
+
+**1.3 Primeiro Programa em C**
+   - Escrevendo, compilando e executando um programa "Hello World" em C.
+   - Exploração da estrutura básica de um programa C.
+
+**Aula 2: Variáveis e Tipos de Dados**
+
+Nesta aula, vamos aprender sobre variáveis e tipos de dados em C.
+
+**2.1 Variáveis e Tipos de Dados**
+   - Declaração de variáveis.
+   - Tipos de dados básicos (int, float, char).
+   - Modificadores de tipo (long, short, signed, unsigned).
+
+**2.2 Operadores**
+   - Operadores aritméticos, de atribuição e de comparação.
+   - Operadores lógicos e bit a bit.
+
+**2.3 Estruturas de Controle**
+   - Condicional: if, else if, else.
+   - Laços de repetição: while, for.
+
+**Aula 3: Arrays e Funções**
+
+Nesta aula, vamos explorar arrays e funções em C.
+
+**3.1 Arrays**
+   - Declaração e inicialização de arrays.
+   - Manipulação de arrays.
+
+**3.2 Funções**
+   - Declaração e definição de funções.
+   - Passagem de parâmetros.
+   - Funções recursivas.
+
+**Aula 4: Ponteiros e Alocação Dinâmica de Memória**
+
+Nesta aula, abordaremos ponteiros e alocação dinâmica de memória em C.
+
+**4.1 Ponteiros**
+   - O que são ponteiros.
+   - Operações com ponteiros.
+   - Ponteiros para funções.
+
+**4.2 Alocação Dinâmica de Memória**
+   - Funções malloc, calloc, realloc e free.
+   - Gerenciamento de memória dinâmica.
+
+**Aula 5: Estruturas e Ponteiros para Estruturas**
+
+Nesta aula, aprenderemos sobre estruturas e ponteiros para estruturas.
+
+**5.1 Estruturas**
+   - Definição de estruturas.
+   - Declaração e uso de estruturas.
+
+**5.2 Ponteiros para Estruturas**
+   - Manipulação de estruturas por meio de ponteiros.
+
+**Aula 6: Arquivos em C**
+
+Nesta aula, vamos explorar como trabalhar com arquivos em C.
+
+**6.1 Operações de Arquivo**
+   - Abertura, leitura e escrita de arquivos.
+   - Fechamento de arquivos.
+
+**6.2 Manipulação de Arquivos Binários e de Texto**
+   - Diferenças entre arquivos binários e de texto.
+   - Manipulação de arquivos de texto e binários.
+
+**Aula 7: Tópicos Avançados**
+
+Nesta aula final, discutiremos tópicos avançados que podem ser úteis ao trabalhar em projetos de programação C mais complexos. Alguns tópicos a considerar são:
+
+- Threads e concorrência.
+- Comunicação entre processos.
+- Manipulação de sinais.
+- Uso de bibliotecas externas.
+- Otimização de código.
